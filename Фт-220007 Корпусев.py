fc2aab304f3a32f0f0a83c4f57f966ab09d88033
1glam1 <102163352+1glam1@users.noreply.github.com>
2023-10-27 01:03:27 +0500

    Добавлена проверка ввода количества критериев

--- Фт-220007 Корпусев.py
+++ Фт-220007 Корпусев.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def matrix(n):
     M = np.ones([n, n]) # Создаем матрицу
-    for i in range(1, n):
-        for j in range(1, n):
+    for i in range(0, n):
+        for j in range(0, n):
             if i < j:
                 mij = input('Введите коэффициент сравнения критерия {0} и критерия {1}: '.format(i,j))
                 M[i, j] = float(mij)
@@ -14,16 +14,20 @@ def matrix(n):
     norm_vector = vector / vector.sum() # пронормируем вектор
     return norm_vector
 
-while True: # Проверка ввода
+while True: # Проверка ввода количества критерий
     try:
         number = int(input('Введите количество критериев: ')) 
-        norm_vector = matrix(number) 
-        print(norm_vector)
-        # выводим результат
-        print('Весовые коэффициенты')
-        for x in range(len(norm_vector)): 
-            res = norm_vector[x]
-            print(f'{res:.2f}'[:-6]) # округление до сотых
+        while True: # Проверка ввода коэффициента
+            try:
+                norm_vector = matrix(number) 
+                # выводим результат
+                print('Весовые коэффициенты')
+                for x in range(len(norm_vector)): 
+                    res = norm_vector[x]
+                    print(f'{res:.2f}'[:-6]) # округление до сотых
+                break
+            except ValueError: 
+                print('Введите число(без символов)!')
         break
-    except ValueError: 
+    except ValueError:
         print('Введите число(без символов)!')
